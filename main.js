54acea4867af4177c3002b0ce217ae7aed37eabe
TheDarkWarrior115 <115947724+TheDarkWarrior115@users.noreply.github.com>
2022-12-04 12:37:03 -0700

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert("Hello");
+a = document.createElement("input");
+b = document.createElement("pre");
+c = document.createElement("style");
+
+c.innerHTML=".cmd {color:white;background-color:black}";
+b.innerHTML = "Hello User: If You Are Lost Type Help For A List Of Commands";
+a.className = "cmd";
+b.className = "cmd";
+
+
+const cmd = {};
+cmd.cmdobj = a;
+cmd.logobj = b;
+cmd.commands = {};
+cmd.commands.help = function(x){
+		
+	}
+cmd.run = function() {};
+cmd.log = function() {};
