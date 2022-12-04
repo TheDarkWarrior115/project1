f714b59f679850d770f5e7b5ed37c07bd717b629
TheDarkWarrior115 <115947724+TheDarkWarrior115@users.noreply.github.com>
2022-12-04 13:09:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -9,11 +9,40 @@ b.className = "cmd";
 
 
 const cmd = {};
+cmd.logreturn = true;
 cmd.cmdobj = a;
 cmd.logobj = b;
 cmd.commands = {};
 cmd.commands.help = function(x){
-		
+		if(x){
+			if(x===color){cmd.log("",true);}
+		}else(
+			cmd.log("<span style='font-weight: bold;'>List Of Commands:</span>",true);
+			cmd.log("Help: shows this list, with parameter shows syntax for specific command",true);
+			cmd.log("	example: Help Color; shows syntax for color command",true);
+			cmd.log("",true);
+		)
 	}
-cmd.run = function() {};
-cmd.log = function() {};
+cmd.run = function(x) {
+		var command = cmd.cmdobj.value;
+		if(x){var command = x;};
+		if(command.includes(" ")===true){
+			cmd1 = command.split(" ");
+			if(cmd1.at(2)===""){
+				1+1;
+			}else(
+			
+			)
+		}
+		if(command.includes(" ")===false){
+			
+		}
+		
+	};
+cmd.log = function(x,ignore) {
+		if(cmd.logreturn === true ){
+			cmd.logobj.innerHTML += "<br>" + x ;
+		}
+		if(ignore === true){cmd.logobj.innerHTML += "<br>" + x;}
+		
+	};
