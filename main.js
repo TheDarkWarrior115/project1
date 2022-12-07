4b1ee3c08bee21c673c0931b9e50b70adde560ba
TheDarkWarrior115 <115947724+TheDarkWarrior115@users.noreply.github.com>
2022-12-07 16:23:36 -0700

    1

--- main.js
+++ main.js
@@ -1,72 +1,36 @@
-//creates elements
-a = document.createElement("input");
-b = document.createElement("pre");
-c = document.createElement("style");
-//Edits Element Created Above
-c.innerHTML=".cmd {color:white;background-color:black}";
-b.innerHTML = "Hello User: If You Are Lost Type Help For A List Of Commands";
+var a = document.createElement("input");
+var b = document.createElement("pre");
+var c = document.createElement("style");
+
+b.innerHTML = "Hello, type help for a list of commands";
+c.innerHTML = ".cmd {color:white;background-color:black;}";
 a.className = "cmd";
 b.className = "cmd";
-//cmd is created
-const cmd = {};
-//log return is set to true, user will be able to turn this of, however help commands will still log;
-cmd.logreturn = true;
-//creates objects on cmd object
-cmd.cmdobj = a;
-cmd.logobj = b;
-//commands object created
+
+cmd = {};
 cmd.commands = {};
-//help command created
-cmd.commands.help = function(x){
-		//if x parameter it will use it and display command syntax, otherwise will display whole list
-		if(x){
-			if(x===color){cmd.log("",true);}
-		}else{
-			cmd.log("<span style='font-weight: bold;'>List Of Commands:</span>");
-			cmd.log("Help: shows this list; if parameter shows command syntax;",true);
-			cmd.log("",true);
-		}
-	}
-//created run function
+cmd.cmdobj=a;
+cmd.logobj=b;
+cmd.val = "";
 cmd.run = function(x) {
-		//gets value of input
-		var command = cmd.cmdobj.value;
-		//if x parameter sets command to the x parameter
-		if(x){var command = x;};
-		//if command includes spaces test for parameters,
-		if(command.includes(" ")===true){
-			cmd1 = command.split(" ");
-			if(cmd1.at(2)===""){
-				hasspace = false; //if accidental space than returns to no parameters ln
-			}else{//if it has parameters it will do this
-				//cmd2 = parameters, cmd 1 = command name
-				cmd2 = command.replace("cmd1.at(1)");
-				cmd1 = cmd1.at(0);
-				cmd1 = cmd1.toLowerCase();
-				if(cmd1==="help"){cmd.commands.help(cmd2);};
-			}
+		cmd.val = cmd.cmdobj.value;
+		if(x){cmd.val = x};
+		cmd.val = cmd.val.trim();
+		if(cmd.val.includes(" ")){
+			cmd1 = cmd.val.split(" ").at(0);
+			cmd2 = cmd.val.replace(cmd1,"");
+			
+		}else{
+			if(cmd.val===){
+			   
+			}else if(){
+				
+			}else if(){
+				
+			}else
+			
 		}
-		//tests for no parameters
-		if(command.includes(" ")===false){
-		noparameters=true;
-		};
-		if(hasspace){
-		noparameters=true;
-		command = command.replace(" ","");
-		command = command.toLowerCase();
-		};
-		if(noparameters===true){//if no parameters it will do this
-			if(command==="help"){cmd.commands.help()};
-		}
-		
 	};
-cmd.log = function(x,ignore) {
-		if(cmd.logreturn === true ){
-			cmd.logobj.innerHTML += "<br>" + x ;
-		}
-		if(ignore === true){cmd.logobj.innerHTML += "<br>" + x;}
-		
-	};
-document.body.appendChild(b);
-document.body.appendChild(a);
-document.head.appendChild(c);
+cmd.log = function(x) {
+	if(x){cmd.logobj.innerHTML += "<br>"+x+""};
+};
